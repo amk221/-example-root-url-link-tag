6b33286e3e3f6da53661eb7996036527a17c618e
Andrew Kirwin <github@andrewkirwin.co.uk>
2024-01-24 00:36:15 +0000

    reproduction

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,13 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    fingerprint: {
+      enabled: true,
+      extensions: ['css', 'js', 'woff2'],
+
+      // Toggle this to show xyz is used on the woff2 link tag in index.html
+      // prepend: `xyz/`,
+    },
   });
 
   return app.toTree();
